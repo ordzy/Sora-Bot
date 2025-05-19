5affdd5f12c5d0a8ba6e79e44a7593a753d16a24
365 <90153309+365-py@users.noreply.github.com>
2025-05-18 19:56:56 -0600

    Added a new idclass.ts
    - Added welcomeChannelId

--- idclass.ts
+++ idclass.ts
@@ -25,8 +25,12 @@ class IDClass {
     return isTest ? "1355635959860826348" : "1353519265684127744";
   }
 
+   welcomeChannelId() {
+    return isTest ? "1373517403643707464" : "1293547438283558935";
+  }
+  
   restrictedCategory() {
-    return isTest ? "1355635959676538930" : "1293432337740075118";
+    return isTest ? "1355635958401470526" : "1293432337740075118";
   }
 
   roleMods() {
